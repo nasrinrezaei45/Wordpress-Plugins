9ed5f5e844acd9298a35a3d820bc03ecb66d6a1f
nasrin rezaei <nasrinrezaei45@gmail.com>
2021-02-22 09:29:22 +0330

    add shepacom-paiddownloads 	new file:   shepacom-paiddownloads/class-paid-downloads-shepacom.php 	new file:   shepacom-paiddownloads/css/style.css 	new file:   shepacom-paiddownloads/img/btn_buynow.png 	new file:   shepacom-paiddownloads/img/delete.png 	new file:   shepacom-paiddownloads/img/download.png 	new file:   shepacom-paiddownloads/img/download01.png 	new file:   shepacom-paiddownloads/img/downloadlink.png 	new file:   shepacom-paiddownloads/img/edit.png 	new file:   shepacom-paiddownloads/img/linkhistory.png 	new file:   shepacom-paiddownloads/img/transactions.png 	new file:   shepacom-paiddownloads/index.php 	new file:   shepacom-paiddownloads/languages/default.po 	new file:   shepacom-paiddownloads/languages/paiddownloads-fr_FR.mo 	new file:   shepacom-paiddownloads/languages/paiddownloads-fr_FR.po

---
+++ shepacom-paiddownloads/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: درگاه پرداخت و پرداخت یار Shepa.com - افزونه دانلود به ازای پرداخت
+Version: 1.0.0
+Description: تنظیمات درگاه پرداخت Shepa.com برای افزونه دانلود به ازای پرداخت
+Plugin URI: https://shepa.com/
+Author: Nasrin Rezaei nasrinrezaei45@gmail.com 09133239584
+Author URI: https://wp-safe.ir/
+*/
+
+require_once('class-paid-downloads-shepacom.php');
