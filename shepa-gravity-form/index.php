c72f1e2e0b8502fafe0009a17d2de6c38e708cc7
nasrin rezaei <nasrinrezaei45@gmail.com>
2021-02-22 09:34:47 +0330

    add shepa-gravity-form 	new file:   shepa-gravity-form/chart.php 	new file:   shepa-gravity-form/database.php 	new file:   shepa-gravity-form/index.php 	new file:   shepa-gravity-form/pay.php

---
+++ shepa-gravity-form/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: درگاه پرداخت و کیف پول الکترونیک shepa.com - افزونه Gravity Forms
+Plugin URI: http://gravityforms.ir/
+Description: افزونه درگاه پرداخت شپا برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.3.0
+Author: Nasrin Rezaei <nasrinrezaei45@gmail.com> 09133239584
+Author URI: http://wp-safe.ir/
+*/
+if (!defined('ABSPATH')) {
+    exit;
+}
+require_once('pay.php');
+
