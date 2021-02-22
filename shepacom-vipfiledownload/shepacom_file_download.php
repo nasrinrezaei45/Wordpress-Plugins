4d8128b934721c35885bcca81f0375168f4c763c
nasrin rezaei <nasrinrezaei45@gmail.com>
2021-02-22 09:31:05 +0330

    add shepacom-vipfiledownload 	new file:   shepacom-vipfiledownload/css/style.css 	new file:   shepacom-vipfiledownload/func_shortcode.php 	new file:   shepacom-vipfiledownload/shepacom_file_download.php 	new file:   shepacom-vipfiledownload/shepacom_func.php

---
+++ shepacom-vipfiledownload/shepacom_file_download.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه پرداخت و کیف پول الکترونیک shepa.com - افزونه VIP Subscribe & File download
+Version: 1.1.0
+Description: افزونه فروشگاه دانلود فایل در ازای پرداخت و اشتراک ویژه
+Plugin URI: https://shepa.com/
+Author: Nasrin Rezaei nasrinrezaei45@gmail.com 09133239584
+Author URI: https://wp-safe.ir/
+*/
+
+include_once('shepacom_func.php');
